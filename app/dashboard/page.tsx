6b7cbccb17642f57b0e0e64fc4fa09018965e996
mustapha <mustaphaamoutaki@gmail.com>
2026-04-29 13:38:02 +0100

    feat: create new spinner button

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+import Spinner from "@/components/ui/spinnerUi";
+
 export default function Dashboard() {
-  return <div>Dashboard Home 📊</div>
+  return <div>
+    Dashboard Home 📊
+
+    <div className="flex justify-center items-center gap-6 mt-6 mb-6">
+      <Spinner />
+     
+    </div>
+  </div>
 }
