bb535ee06ea8ed857575df30182723094b766b84
mustapha <mustaphaamoutaki@gmail.com>
2026-04-30 11:11:34 +0100

    feat: create Clients-OverViewDtoResponse type

---
+++ src/types/ClientStatsOverView.ts
@@ -0,0 +1,7 @@
+export interface ClientStatsOverView{
+    totalClients: number,
+    totalActiveClients: number,
+    totalClientsToday: number,
+    avgSatisfactionScore: number,
+    lowSatisfactionClients: number
+}
