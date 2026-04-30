98c291b3b7fdda75b72cff51b92718bd88e95101
mustapha <mustaphaamoutaki@gmail.com>
2026-04-30 11:14:19 +0100

    feat: use react query for caching and refetching

--- src/hooks/useAdminDashboardOverview.ts
+++ src/hooks/useAdminDashboardOverview.ts
@@ -8,11 +8,11 @@ export const useAdminDashboardOverview = ()=>{
         queryKey: ["admin_dashboard_overview"],
         queryFn: getAdminDashboardOverview,
 
-        // caching config 
+        // caching config  
         staleTime: 1000 * 60 * 2, // 2 minutes fresh data
         gcTime: 1000 * 60 * 10, // keep cached for 10 minutes (even if inactive)
 
-        // refetch configs
+        // refetch configs 
         refetchOnWindowFocus: true, // refetch when window regains focus
         refetchOnReconnect: true // refetch when browser regains connection
     });
