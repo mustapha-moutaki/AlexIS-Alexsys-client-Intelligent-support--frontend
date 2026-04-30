ef005d9974e6633fb3117d16f07352073d525059
mustapha <mustaphaamoutaki@gmail.com>
2026-04-30 11:10:49 +0100

    feat: create admin dashboardOverViewDtoResponse type

---
+++ src/types/AdminDashboardOverView.ts
@@ -0,0 +1,43 @@
+export interface AdminDashboardOverview {
+  totalClients: number;
+  totalClientsToDay: number;
+  activeClients: number;
+
+  avgSatisfactionScore: number | null;
+  lowSatisfactionClient: number;
+
+  totalAgents: number;
+  totalAvailableAgents: number;
+  totalBusyAgents: number;
+  overloadAgents: number;
+
+  averageResolutionTime: number;
+  avgPerformanceRating: number;
+  bestAgent: string | null;
+
+  totalTickets: number;
+  totalActiveTickets: number;
+  totalResolvedTickets: number;
+  totalClosedTickets: number;
+
+  avgResolutionTime: number | null;
+  highPriorityTickets: number;
+  totalTicketsToday: number;
+
+  totalCategories: number;
+
+  countMyTotalTickets: number;
+  countMyOpenTickets: number;
+  countMyInProgressTickets: number;
+  countMyResolvedTickets: number;
+  myClosedTickets: number;
+
+  myTicketsCreatedToday: number;
+  myHighPriorityTickets: number;
+  myMediumPriorityTickets: number;
+  myLowPriorityTickets: number;
+
+  ticketsNeedingAttention: number;
+  riskDetectedAgent: string | null;
+  avgLoadPerAgent: number;
+};
