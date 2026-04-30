31bcbd83a0e5d7d9abfce8981d44db7ad91fc7eb
mustapha <mustaphaamoutaki@gmail.com>
2026-04-30 11:13:34 +0100

    feat: create custom hook for admin dashaboard overView

---
+++ src/hooks/useAdminDashboardOverview.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { getAdminDashboardOverview } from "../features/auth/services/dashaboard.service"
+
+
+
+export const useAdminDashboardOverview = ()=>{
+    return useQuery({
+        queryKey: ["admin_dashboard_overview"],
+        queryFn: getAdminDashboardOverview,
+
+        // caching config 
+        staleTime: 1000 * 60 * 2, // 2 minutes fresh data
+        gcTime: 1000 * 60 * 10, // keep cached for 10 minutes (even if inactive)
+
+        // refetch configs
+        refetchOnWindowFocus: true, // refetch when window regains focus
+        refetchOnReconnect: true // refetch when browser regains connection
+    });
+}
