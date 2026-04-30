d51cc0ed0c7504e09f36ef4a728ef6684be1566c
mustapha <mustaphaamoutaki@gmail.com>
2026-04-30 11:10:23 +0100

    feat: create constants for dashaboard - endpoints grouping

---
+++ src/shared/constants/endpoints.ts
@@ -0,0 +1,17 @@
+export const DASHBOARD_ENDPOINTS = {
+  ADMIN: {
+    OVERVIEW: "dashboard/statistics/overview",
+    AGENTS: "dashboard/statistics/agents",
+    CLIENTS: "dashboard/statistics/clients",
+    TICKETS: "dashboard/statistics/tickets",
+    CATEGORIES: "dashboard/statistics/categories",
+  },
+
+  CLIENT: {
+    OVERVIEW: "dashboard/client/overview",
+  },
+
+  AGENT: {
+    OVERVIEW: "dashboard/agent/overview",
+  },
+};
