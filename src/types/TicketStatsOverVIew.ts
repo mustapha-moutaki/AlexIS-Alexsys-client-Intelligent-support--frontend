ffbd6a6f84da3bc88735bc8a589ab09241a61147
mustapha <mustaphaamoutaki@gmail.com>
2026-04-30 11:11:17 +0100

    feat: create TicketsOverViewDtoResponse type

---
+++ src/types/TicketStatsOverVIew.ts
@@ -0,0 +1,9 @@
+export interface TicketStatsOverView{
+    totalTickets: number;
+    totalActiveTickets: number;
+    totalResolvedTickets: number;
+    totalClosedTickets: number;
+    avgResolutionTime: number;
+    highPriorityTickets: number;
+    totalTicketsToday: number;
+}
