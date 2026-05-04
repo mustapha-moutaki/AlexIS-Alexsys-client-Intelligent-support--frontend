9717fec0751e033ec0aeb55ee2be44e44a95ab98
mustapha <mustaphaamoutaki@gmail.com>
2026-05-04 17:10:35 +0100

    feat: new dashaboard loading component

---
+++ components/ui/SimpleSpinner.tsx
@@ -0,0 +1,7 @@
+export default function SimpleSpinner(){
+   return (
+      <div className="h-full w-full flex items-center justify-center bg-[#0d0014]">
+        <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-[#51c2de]"></div>
+      </div>
+    );
+}
