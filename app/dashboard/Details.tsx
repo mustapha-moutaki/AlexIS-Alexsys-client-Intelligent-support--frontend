6312b9bf2a799ec2c3deabc601bd705c68bb3e7e
mustapha <mustaphaamoutaki@gmail.com>
2026-05-04 17:05:56 +0100

    feat: add filedSkeleton

--- app/dashboard/Details.tsx
+++ app/dashboard/Details.tsx
@@ -1,9 +1,20 @@
 "use client";
+
+import FieldSkeleton from "@/components/ui/FieldSkeleton";
+import GlassComponent from "@/components/GlassComponent";
 export default function Details() {
   return (
     <>
       {/* PASTE ALL YOUR DASHBOARD UI HERE */}
       <h2>Details</h2>
+      <FieldSkeleton/>
+      <div className="flex space-x-2">
+           <FieldSkeleton/>
+      <FieldSkeleton/>
+      </div>
+   
+    <GlassComponent/>
+
     </>
   );
 }
