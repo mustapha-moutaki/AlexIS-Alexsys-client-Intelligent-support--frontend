ee5920d11611e23d8f8185a0b78ae1da30c082c7
mustapha <mustaphaamoutaki@gmail.com>
2026-05-04 17:12:05 +0100

    create user custom hook

---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query"
+import { getUsers } from "../features/auth/services/user.service"
+
+type Params={
+  page?: number;
+  sortBy?: string;
+  direction?: "asc" | "desc";
+  role?: string;
+  includeDeleted?: boolean;
+}
+export const useUsers = (params:Params)=>{
+    return useQuery({
+        queryKey:["users",params],
+        queryFn: ()=>getUsers(params),
+
+         // caching config  
+        staleTime: 1000 * 60 * 2, // 2 minutes fresh data
+        gcTime: 1000 * 60 * 10, // keep cached for 10 minutes (even if inactive)
+
+        // refetch configs 
+        refetchOnWindowFocus: true, // refetch when window regains focus
+        refetchOnReconnect: true // refetch when browser regains connection
+    })
+}
