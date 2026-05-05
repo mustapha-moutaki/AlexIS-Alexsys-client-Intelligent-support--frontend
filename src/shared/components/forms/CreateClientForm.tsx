881c2d96f6f6b577fe7ae280c6f63f3f1be478c3
mustapha <mustaphaamoutaki@gmail.com>
2026-05-05 13:13:39 +0100

    feat: create client Dto request type

---
+++ src/shared/components/forms/CreateClientForm.tsx
@@ -0,0 +1,10 @@
+export default function CreateClientForm({onSubmit, isLoading}:{
+    onSubmit: (data: any) => void;
+    isLoading: boolean;
+}) {
+    return (
+        <div>
+            <h1>Create Client Form</h1>
+        </div>
+    );
+}
