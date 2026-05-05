2a07d93bf08f32c326d3f254e848aaa42aed2131
mustapha <mustaphaamoutaki@gmail.com>
2026-05-05 13:11:12 +0100

    feat: imporvoe loading -rm bg

--- components/ui/SimpleSpinner.tsx
+++ components/ui/SimpleSpinner.tsx
@@ -1,6 +1,6 @@
 export default function SimpleSpinner(){
    return (
-      <div className="h-full w-full flex items-center justify-center bg-[#0d0014]">
+      <div className="h-full w-full flex items-center justify-center bg-transparent">
         <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-[#51c2de]"></div>
       </div>
     );
