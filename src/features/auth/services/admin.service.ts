7fb5e6110e1870959f4d5318a19c6ccfe66e8bb9
mustapha <mustaphaamoutaki@gmail.com>
2026-05-06 13:30:08 +0100

    fix: add cotent type : multipart/form-data

--- src/features/auth/services/admin.service.ts
+++ src/features/auth/services/admin.service.ts
@@ -7,7 +7,33 @@ import { User } from "@/src/types/User";
 
 
 
-export const createAdminUser = async(user:CreateAdminRequest)=>{
-    const res = await api.post<ApiResponse<User>>("/users",user);
+
+export const createAdminUser = async (user: CreateAdminRequest) => {
+
+    
+    const formData = new FormData();
+
+    formData.append("firstName", user.firstName);
+    formData.append("lastName", user.lastName);
+    formData.append("username", user.username);
+    formData.append("email", user.email);
+    formData.append("password", user.password);
+    formData.append("phoneNumber", user.phoneNumber);
+
+    if (user.profilePicture) {
+        formData.append("profilePicture", user.profilePicture);
+    }
+
+    const res = await api.post<ApiResponse<User>>(
+        "/users",
+        formData,
+        {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        }
+    );
+   
+
     return res.data;
-}
+};
