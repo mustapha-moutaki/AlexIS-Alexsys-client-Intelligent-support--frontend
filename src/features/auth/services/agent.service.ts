51278d3e420aa8546b12174e857720fae017cc9b
mustapha <mustaphaamoutaki@gmail.com>
2026-05-06 13:30:21 +0100

    fix: add cotent type : multipart/form-data

--- src/features/auth/services/agent.service.ts
+++ src/features/auth/services/agent.service.ts
@@ -5,11 +5,31 @@ const AGENTENDPOINT = "/agents";
 
 
 export const createAgent = async (data: AgentCreateRequest) => {
-    try {
-        const response = await api.post(`${AGENTENDPOINT}`, data);
-        return response;
-    } catch (error) {
-        console.error("Create agent error:", error);
-        throw error;
+
+
+    const formData = new FormData();
+    formData.append("firstName", data.firstName);
+    formData.append("lastName", data.lastName);
+    formData.append("username", data.username);
+    formData.append("email", data.email);
+    formData.append("password", data.password);
+    formData.append("phoneNumber", data.phone);
+    formData.append("specialization", data.specialization);
+    formData.append("averageResolutionTime", data.averageResolutionTime.toString());
+    formData.append("performanceRating", data.performanceRating.toString());
+    formData.append("availabilityStatus", data.availabilityStatus);
+    formData.append("level", data.level);
+    
+    if(data.profilePicture){
+        formData.append("profilePicture", data.profilePicture);
     }
+    
+        const res = await api.post(`${AGENTENDPOINT}`, formData,{
+            headers:{
+                "Content-Type":"multipart/form-data",
+            }
+        });
+        return res;
+       
+   
 }
