49ddbeda7d8e62a078f8dec0d29f4809aa52bc70
mustapha <mustaphaamoutaki@gmail.com>
2026-05-09 12:00:00 +0100

    light mode

--- src/types/User.ts
+++ src/types/User.ts
@@ -4,6 +4,7 @@ export interface User {
     email: string;
     firstName?: string;
     lastName?: string;
-    roles?: string[];
+    role?: string;
     profilePicture?: string;
+    phoneNumber?: string;
 }
