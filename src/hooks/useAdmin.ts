497d256b2cbd18a16f20c2ac214e66788c9df69b
mustapha <mustaphaamoutaki@gmail.com>
2026-05-09 12:00:00 +0100

    light mode

--- src/hooks/useAdmin.ts
+++ src/hooks/useAdmin.ts
@@ -1,7 +1,8 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createAdminUser } from "@/src/features/auth/services/admin.service";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createAdminUser, getAdmins, updateAdmin } from "@/src/features/auth/services/admin.service";
 
 import toast from "react-hot-toast";
+import { UserEditRequest } from "../types/UserEditRequest";
 
 export const useCreateAdmin = () => {
   const queryClient = useQueryClient();
@@ -23,4 +24,45 @@ export const useCreateAdmin = () => {
     toast.error("Failed to create admin");
   },
 });
+};
+
+export const useAdmins = (page: number,enabled:boolean= true) => {
+  return useQuery({
+    queryKey: ["admins", page],
+    queryFn: () => getAdmins(page),
+    enabled:enabled,
+  });
+};
+
+
+
+
+export const useUpdateAdmin = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    // We pass an object containing both the id and the update data
+    mutationFn: ({ id, data }: { id: string; data: UserEditRequest }) => updateAdmin(id, data),
+
+    onMutate: () => {
+      toast.loading("Updating admin profile...");
+    },
+
+    onSuccess: (data, variables) => {
+      toast.dismiss();
+      toast.success("Admin updated successfully");
+
+      // 1. Invalidate the general admins list to trigger a refresh
+      queryClient.invalidateQueries({ queryKey: ["admins"] });
+
+      // 2. Invalidate the specific user query if you have a "useUserById" hook
+      queryClient.invalidateQueries({ queryKey: ["user", variables.id] });
+    },
+
+    onError: (error: any) => {
+      toast.dismiss();
+      const errorMessage = error?.response?.data?.message || "Failed to update admin";
+      toast.error(errorMessage);
+    },
+  });
 };
