72bedbb9534d32e43417f15edccd493058ad1ab9
mustapha <mustaphaamoutaki@gmail.com>
2026-05-10 12:00:00 +0100

    light mode

---
+++ src/types/Agent.tsx
@@ -0,0 +1,16 @@
+export interface Agent{
+    id: number,
+    firstName: string,
+    lastName: string,
+    username: string
+    email: string,
+    phoneNumber: string,
+    profilePicture: string,
+    active: boolean,
+    deleted: boolean,
+    specialization: string,
+    averageResolutionTime: string,
+    performanceRating: number,
+    availabilityStatus: string,
+    level: string
+}
