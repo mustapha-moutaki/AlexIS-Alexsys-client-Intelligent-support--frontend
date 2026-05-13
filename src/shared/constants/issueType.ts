118a688d32a60d4cf29dd8d9e2f65524ca01207f
mustapha <mustaphaamoutaki@gmail.com>
2026-05-13 15:19:57 +0100

    feat: create issueType type

---
+++ src/shared/constants/issueType.ts
@@ -0,0 +1,10 @@
+export const issueType = [
+
+    {label:"Incident",value:"INCIDENT"},
+    {label:"Bug",value:"BUG"},
+    {label:"Request",value:"REQUEST"},
+    {label:"UI",value:"UI"},
+    {label:"UX",value:"UX"},
+    {label:"Other",value:"OTHER"},
+]
+
