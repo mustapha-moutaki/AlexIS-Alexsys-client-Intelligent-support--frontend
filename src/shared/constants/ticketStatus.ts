91508339cff6995a3ea2b865016581782798b7b5
mustapha <mustaphaamoutaki@gmail.com>
2026-05-13 15:20:39 +0100

    feat: create ticketStatus type

---
+++ src/shared/constants/ticketStatus.ts
@@ -0,0 +1,10 @@
+export const ticketStatus = [
+
+    {label:"Open",value:"OPEN"},
+    {label:"Assigned",value:"ASSIGNED"},
+    {label:"In Progress",value:"IN_PROGRESS"},
+    {label:"Resolved",value:"RESOLVED"},
+    {label:"Closed",value:"CLOSED"},
+    {label:"Reopened",value:"REOPEN"}
+]
+    
