99d85de38de04707a20ece6a593b327ad04ed032
mustapha <mustaphaamoutaki@gmail.com>
2026-05-13 15:18:54 +0100

    feat: create ticket for

---
+++ src/types/TicketFormData.ts
@@ -0,0 +1,10 @@
+export interface TicketFormData {
+      title: string;
+  description: string;
+  status: string;
+  priority: string;
+  issueType: string;
+  categoryId: string;
+  clientId: string;
+  assignedToId: string;
+}
