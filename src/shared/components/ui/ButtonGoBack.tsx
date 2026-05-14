1a4f1f0c7a5dcda15211210d27d701f8d58eb812
mustapha <mustaphaamoutaki@gmail.com>
2026-05-14 10:45:53 +0100

    feat: make the cursor pointer

--- src/shared/components/ui/ButtonGoBack.tsx
+++ src/shared/components/ui/ButtonGoBack.tsx
@@ -10,7 +10,7 @@ const ButtonGoBack = () => {
   return (
     <button
       onClick={() => router.back()}
-      className="flex items-center justify-center rounded-xl transition-all hover:bg-gray-50 hover:scale-[1.03]"
+      className="flex items-center justify-center rounded-xl transition-all hover:bg-gray-50 hover:scale-[1.03] cursor-pointer"
       style={{
         width: 34,
         height: 34,
