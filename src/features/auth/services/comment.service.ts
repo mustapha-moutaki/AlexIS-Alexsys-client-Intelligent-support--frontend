5becb8588c3cc5e23cc113d5eed067e2f51df4d4
mustapha <mustaphaamoutaki@gmail.com>
2026-05-16 17:04:42 +0100

    fix: remove sendin- user id in the frontend

---
+++ src/features/auth/services/comment.service.ts
@@ -0,0 +1,84 @@
+import api from "@/src/lib/api";
+import useAuthStore from "@/src/store/authStore";
+import { ApiResponse } from "@/src/types/ApiResponse";
+import { CommentRequest } from "@/src/types/CommentRequest";
+import { CommentResponse } from "@/src/types/CommentResponse";
+
+const COMMENT_ENDPOINT = "/comments";
+
+// get all comments
+
+type Params ={
+    page?:number;
+    size?:number;
+}
+export const getAllComments = async(params:Params)=>{
+   try{
+    const res = await api.get<ApiResponse<CommentResponse[]>>(`${COMMENT_ENDPOINT}`);
+    return res.data.data;
+   }catch(error){
+    throw error;
+   }
+}
+// get comment by id
+export const getCommentById = async(id:string)=>{
+   try{
+    const res = await api.get<ApiResponse<CommentResponse>>(`${COMMENT_ENDPOINT}/${id}`);
+    return res.data.data;
+   }catch(error){
+    throw error;
+   }
+}
+
+
+// get comments by ticket id
+export const getCommentsByTicketId = async(ticketId:string)=>{
+   try{
+    const res = await api.get<ApiResponse<CommentResponse[]>>(`${COMMENT_ENDPOINT}/ticket/${ticketId}`);
+    return res.data.data;
+   }catch(error){
+    throw error;
+   }
+}
+
+// delete comment
+export const deleteComment = async(id:string)=>{
+   try{
+    const res = await api.delete<ApiResponse<CommentResponse>>(`${COMMENT_ENDPOINT}/${id}`);
+    return res.data.data;
+   }catch(error){
+    throw error;
+   }
+}
+
+// update comment
+export const updateComment = async(id:string,comment:CommentResponse)=>{
+   try{
+    const res = await api.put<ApiResponse<CommentResponse>>(`${COMMENT_ENDPOINT}/${id}`,comment);
+    return res.data.data;
+   }catch(error){
+    throw error;
+   }
+}
+
+// create comment
+export const createComment = async(data:{ticketId:number, content:string}):Promise<CommentResponse>=>{
+   //  const user = useAuthStore.getState().user;
+   //  const userId = user?.id as number;
+
+    const commentRequest: CommentRequest = {
+        content: data.content,
+      //   userId: userId,
+        ticketId: data.ticketId
+    };
+
+   try{
+    const res = await api.post<ApiResponse<CommentResponse>>(`${COMMENT_ENDPOINT}`,commentRequest);
+    console.log("comment created successfully");
+    
+    return res.data.data;
+   }catch(error:any){
+    const errorMessage = error.response?.data?.message || "Failed to create comment";
+    throw errorMessage;
+   }
+}
