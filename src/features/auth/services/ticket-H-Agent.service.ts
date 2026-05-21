d2757457fd9bce060d5be39506264c02c3db9a7b
mustapha <mustaphaamoutaki@gmail.com>
2026-05-21 14:25:05 +0100

    feat: add new function to retunr list of the agent assigned ticket

--- src/features/auth/services/ticket-H-Agent.service.ts
+++ src/features/auth/services/ticket-H-Agent.service.ts
@@ -32,3 +32,31 @@ export const getAllTickets= async(status: string)=>{
     }
 
 }
+
+export const getTicketByIdForAgent = async (id: number) => {
+    try {
+        const res = await api.get<ApiResponse<TicketDetailsResponse>>(`${TICKET_BASE_ENDPOINT}/${id}`);
+        if (res.data && res.data.success) {
+            return res.data.data;
+        }
+        throw new Error("Failed to fetch ticket");
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+export const updateTicketStatusAgent = async (ticketId: number)=>{
+    try{
+        const res = await api.put<ApiResponse<any>>(`${TICKET_BASE_ENDPOINT}/${ticketId}`,{
+            status:"RESOLVED"
+        });
+        if(res.data && res.data.success){
+            return res.data.data;
+        }
+        throw new Error("Failed to update ticket status");
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
+}
