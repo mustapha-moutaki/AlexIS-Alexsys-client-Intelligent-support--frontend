172f9e2cc58e933c82a60f931629eacec23be399
mustapha <mustaphaamoutaki@gmail.com>
2026-05-22 15:22:46 +0100

    Update dashboard overview type definitions

--- src/types/ClientDashboardOverview.ts
+++ src/types/ClientDashboardOverview.ts
@@ -1,3 +1,13 @@
 export interface ClientDashboardOverview {
-    
+    countMyClosedTickets: number;
+    countMyOpenTickets: number;
+    countMyResolvedTickets: number;
+    countMyTotalTickets: number;
+    myClosedTickets: number;
+    myHighPriorityTickets: number;
+    myLowPriorityTickets: number;
+    myMediumPriorityTickets: number;
+    ticketsNeedingAttention: number;
+    registrationDate: string;
+    countMyInProgressTickets: number;
 }
