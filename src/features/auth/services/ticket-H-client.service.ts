8e88044d01cfffd416ccda36c80937193aa11b6e
mustapha <mustaphaamoutaki@gmail.com>
2026-06-01 12:31:24 +0100

    feat: add new methods to get the ticekt by the id for client and update the ticet

--- src/features/auth/services/ticket-H-client.service.ts
+++ src/features/auth/services/ticket-H-client.service.ts
@@ -2,6 +2,8 @@ import { CreateClientTicketPayload } from "@/src/types/CreateClientTicketPayload
 import api from "@/src/lib/api";
 import { TicketResponse } from "@/src/types/TicketResponse";
 import { ApiResponse } from "@/src/types/ApiResponse";
+import { TicketDetailsResponse } from "@/src/types/TicketDetailsResponse";
+import { UpdateClientTicketPayload } from "@/src/types/UpdateClientTicketPayload";
 
 
 const TICKET_BASE_URL = "/client/tickets";
@@ -15,4 +17,15 @@ export const createTicketByClient = async (data: CreateClientTicketPayload) => {
     }catch(error){
         throw error;
     }
+}
+
+export const getClientTicketById = async (ticketId: number) =>{
+    const res = await api.get<ApiResponse<TicketDetailsResponse>>(`${TICKET_BASE_URL}/${ticketId}`);
+    return res.data.data;
+}
+
+// client updated his own ticket
+export const updateClientTicket = async(ticketId: number, data: UpdateClientTicketPayload) =>{
+    const res = await api.patch<ApiResponse<TicketResponse>>(`${TICKET_BASE_URL}/${ticketId}`, data);
+    return res.data.data;
 }
